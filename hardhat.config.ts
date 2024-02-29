f5028fc6116f8527461be23e5c6e2409e50cf20e
Salman Dabbakuti <dabbakutisalman@gmail.com>
2024-02-29 15:20:30 +0530

    added: bnb chains to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,36 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
+    bscTestnet: {
+      url: "https://bsc-testnet-dataseed.bnbchain.org",
+      chainId: 97,
+      accounts
+    },
+    bscMainnet: {
+      url: "https://bsc-dataseed.bnbchain.org",
+      chainId: 56,
+      accounts
+    },
+    greenFieldTestnet: {
+      url: "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org",
+      chainId: 5600,
+      accounts
+    },
+    greenFieldMainnet: {
+      url: "https://greenfield-chain-ap.bnbchain.org",
+      chainId: 1017,
+      accounts
+    },
+    opBNBTestnet: {
+      url: "https://opbnb-testnet-rpc.bnbchain.org",
+      chainId: 5611,
+      accounts
+    },
+    opBNBMainnet: {
+      url: "https://opbnb-mainnet-rpc.bnbchain.org",
+      chainId: 204,
+      accounts
+    },
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts
